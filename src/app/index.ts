53de1e6c942a4fb6694624aa85991254ee5f27e9
Andrew Smith <andrews@dev6.com>
2016-06-10 15:57:00 -0400

    Update app folder barrel

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,8 @@
 // The app folder barrel. Contains all of the exports for this folder
 
 export * from './app.component';
+export * from './dashboard.component';
+export * from './hero-detail.component';
+export * from './hero.model';
+export * from './hero.service';
+export * from './heroes.component';
